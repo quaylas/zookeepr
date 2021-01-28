3610bd60860bc8c954480947f69841f88c8bcaea
quaylas <quayla@cxadvisors.com>
2021-01-28 15:56:00 -0700

    integrated with front end

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals.json');
 
@@ -113,6 +114,17 @@ app.post('/api/animals', (req,res) => {
     }
 });
 
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
